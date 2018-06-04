55685a4071245f9ea0bf6e6e74895d64dc47d0ae
Jennifer Hansen <jenn.leigh.hansen@gmail.com>
2018-06-04 16:41:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function produceDrivingRange(num){
-  return function (trip){
+  return function (trip, num){
     if(num > trip ){
-      return `${num-trip} blocks within range.`
+      return `blocks within range by ${num-trip} .`
     } else {
       return `${trip-num} blocks out of range.`
     }
