534fde138efd27d9aa3e2907aa14f2a4b464f4b0
Jennifer Hansen <jenn.leigh.hansen@gmail.com>
2018-06-04 16:44:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ function produceDrivingRange(num){
     let n = parseInt(num)
     let t = parseInt(trip)
     console.log(n)
+    console.log(t)
     if(n > t ){
       return `within range by ${n-t} .`
     } else {
