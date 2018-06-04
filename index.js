839d55a391faab3c5329287df2726910a84b8218
Jennifer Hansen <jenn.leigh.hansen@gmail.com>
2018-06-04 16:53:02 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,20 @@
+function produceDrivingRange(num){
+  console.log(num)
+  return function (start, end){
+    let s = parseInt(start)
+    let e = parseInt(end)
+    let distance = Math.abs(s - e)
+    console.log(distance)
+    if(num > distance ){
+      return `within range by ${num-distance}`
+    } else {
+      return `${distance-num} blocks out of range`
+    }
+  }
+}
+
+function produceTipCalculator(dec){
+  return function(amount){
+    return dec * amount
+  }
+}
