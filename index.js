6e6bd287d687077f10b77643a63a02a916633b60
Jennifer Hansen <jenn.leigh.hansen@gmail.com>
2018-06-04 16:42:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ function produceDrivingRange(num){
   console.log(num)
   return function (trip, num){
     console.log(num)
+    console.log(trip)
     if(num > trip ){
       return `within range by ${num-trip} .`
     } else {
