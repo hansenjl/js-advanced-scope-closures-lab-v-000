5d07afd2b2b3949f70786efa26f96b5e06d5098c
Jennifer Hansen <jenn.leigh.hansen@gmail.com>
2018-06-04 16:46:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 function produceDrivingRange(num){
   console.log(num)
-  return function (trip, num){
-    let n = parseInt(num)
-    let t = parseInt(trip)
-    console.log(n)
-    console.log(t)
-    if(n > t ){
+  return function (start, end){
+    let s = parseInt(start)
+    let e = parseInt(end)
+    let distance = s - e
+
+    if(num > distance ){
       return `within range by ${n-t} .`
     } else {
       return `${t-n} blocks out of range.`
